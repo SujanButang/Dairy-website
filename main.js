9b46d0878e6c6fac998cee5fbe4514b3840dad08
SujanButang <sujanbutang@gmail.com>
2024-02-21 12:50:16 +0545

    small change

--- main.js
+++ main.js
@@ -2,10 +2,18 @@ const mobileNav = document.querySelector("#mobile-nav-bar");
 const menuBtn = document.querySelector("#menu-btn");
 const closeBtn = document.querySelector("#close");
 
+const anchors = document.querySelectorAll("a");
+
 menuBtn.addEventListener("click", () => {
   mobileNav.style.display = "flex";
 });
 
-closeBtn.addEventListener("click",()=>{
+closeBtn.addEventListener("click", () => {
+  mobileNav.style.display = "none";
+});
+
+anchors.forEach((anchor) => {
+  anchor.addEventListener("click", () => {
     mobileNav.style.display = "none";
-})
+  });
+});
